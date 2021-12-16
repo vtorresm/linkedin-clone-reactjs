1a67e9d3765252c859012c9df66ea6330b71272c
vtorresm <vatorresm88@gmail.com>
2021-12-15 21:23:17 -0500

    Implementando el Header y insatalando Material IU

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>LinkedIn CLONE</h1>
+
+      <Header />
     </div>
   );
 }
