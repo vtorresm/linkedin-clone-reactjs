ae8162c425df9038b212176cb9c435c91ec8dc23
vtorresm <vatorresm88@gmail.com>
2021-12-21 22:04:04 -0500

    Probando la app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <h1>LinkedIn CLONE</h1>
+      <h1>LinkedIn CLONE con REACTJS</h1>
 
       <Header />
     </div>
